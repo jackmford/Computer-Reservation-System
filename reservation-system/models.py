21990eaf4b32b240e4f30aadaa54db3073020eb7
jackmford <fordjac@bvu.edu>
2019-05-13 13:26:43 -0500

    models

---
+++ reservation-system/models.py
@@ -0,0 +1,94 @@
+from app import db
+class Users(db.Model):
+    __tablename__='users'
+
+    username = db.Column(db.String())
+    password = db.Column(db.String())
+    computer_ID = db.Column(db.Integer)
+    email = db.Column(db.String())
+
+    def serialize(self):
+        return {
+            'username': self.username,
+            'password': self.password,
+            'computer_ID': self.computer_ID,
+            'email': self.email,
+
+        }
+
+    def __repr__(self):
+        return 'Users'+str(self.serialize())
+
+class Computers(db.Model):
+    __tablename__='computers'
+
+    computer_ID = db.Column(db.Integer, primary_key=True, foreign_key=True)
+    availability = db.Column(db.Integer)
+    checkout_time = db.Column(db.Integer)
+    reservation_end_time = db.Column(db.Integer) from app import db                                                                                                    2
+class Users(db.Model):
+    __tablename__='users'
+
+    username = db.Column(db.String())
+    password = db.Column(db.String())
+    computer_ID = db.Column(db.Integer)
+    email = db.Column(db.String())
+
+    def serialize(self):
+        return {
+            'username': self.username,
+            'password': self.password,
+            'computer_ID': self.computer_ID,
+            'email': self.email,
+
+        }
+
+    def __repr__(self):
+        return 'Users'+str(self.serialize())
+
+class Computers(db.Model):
+    __tablename__='computers'
+
+    computer_ID = db.Column(db.Integer, primary_key=True, foreign_key=True)
+    availability = db.Column(db.Integer)
+    checkout_time = db.Column(db.Integer)
+    reservation_end_time = db.Column(db.Integer) from app import db                                                                                                    2
+class Users(db.Model):
+    __tablename__='users'
+
+    username = db.Column(db.String())
+    password = db.Column(db.String())
+    computer_ID = db.Column(db.Integer)
+    email = db.Column(db.String())
+
+    def serialize(self):
+        return {
+            'username': self.username,
+            'password': self.password,
+            'computer_ID': self.computer_ID,
+            'email': self.email,
+
+        }
+
+    def __repr__(self):
+        return 'Users'+str(self.serialize())
+
+class Computers(db.Model):
+    __tablename__='computers'
+
+    computer_ID = db.Column(db.Integer, primary_key=True, foreign_key=True)
+    availability = db.Column(db.Integer)
+    checkout_time = db.Column(db.Integer)
+    reservation_end_time = db.Column(db.Integer)
+
+		def serialize(self):
+				return{
+						'computer_ID': self.computer_ID,
+						'availability': self.availability,
+						'checkout_time': self.checkout_time,
+						'reservation_end_time': self.reservation_end_time,
+
+				}
+
+		def __repr__(self):
+				return 'Computers'+str(self.serialize())                                 
